07b2ed4aa2cf465a40e4e99cab747c9b63e3c4a5
Nikita <main.acr0matic@gmail.com>
2021-09-20 15:25:55 +0300

    feat: верстка "полезные материалы"

---
+++ src/scripts/slider.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-unused-vars
+
+const useful = new Swiper('.slider-useful', {
+  // loop: true,
+  slidesPerView: 4,
+  spaceBetween: 25,
+});
