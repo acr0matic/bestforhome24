669b7e6d9f811534b922c07e5c8a243393141198
Nikita <main.acr0matic@gmail.com>
2021-10-04 12:48:42 +0300

    feat: верстка калькулятора

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable wrap-iife */
 /* eslint-disable no-undef */
@@ -6,7 +7,14 @@ class CalculatorController {
   constructor(ref) {
     this.calc = ref;
     this.groups = ref.querySelectorAll('.calculator__group');
+    this.select = ref.querySelectorAll('[data-trigger]');
+
     this.radioGroup = _.map(this.groups, (group) => group.querySelectorAll('input[type=radio]'));
+    this.choices = _.map(this.select, (select) => new Choices(select, {
+      searchEnabled: false,
+      itemSelectText: 'Выбрать',
+      noChoicesText: 'Нет элементов',
+    }));
 
     this.radioGroup.forEach((group) => this.UpdateRadio(group));
     this.Events();
@@ -17,6 +25,12 @@ class CalculatorController {
       group.forEach((radio) => {
         radio.addEventListener('change', () => this.UpdateRadio(group));
       });
+    });
+
+    this.select[0].addEventListener('choice', () => {
+      this.choices[1].clearChoices();
+
+      console.log(this.choices[0].getValue());
     });
   }
 
