60b7bdaf1ac768c960fbf4371024200ffb5b69dc
Nikita <main.acr0matic@gmail.com>
2021-10-04 13:39:42 +0300

    refactor: обновление линтера и кода скриптов

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -1,30 +1,23 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable class-methods-use-this */
-/* eslint-disable wrap-iife */
-/* eslint-disable no-undef */
-
 class CalculatorController {
   constructor(ref) {
     this.calc = ref;
     this.groups = ref.querySelectorAll('.calculator__group');
     this.select = ref.querySelectorAll('[data-trigger]');
 
-    this.radioGroup = _.map(this.groups, (group) => group.querySelectorAll('input[type=radio]'));
+    this.radioGroup = _.invokeMap(this.groups, 'querySelectorAll', 'input[type=radio]');
     this.choices = _.map(this.select, (select) => new Choices(select, {
       searchEnabled: false,
       itemSelectText: 'Выбрать',
       noChoicesText: 'Нет элементов',
     }));
 
-    this.radioGroup.forEach((group) => this.UpdateRadio(group));
+    _.forEach(this.radioGroup, (group) => this.UpdateRadio(group))
     this.Events();
   }
 
   Events() {
-    this.radioGroup.forEach((group) => {
-      group.forEach((radio) => {
-        radio.addEventListener('change', () => this.UpdateRadio(group));
-      });
+    _.forEach(this.radioGroup, (group) => {
+      _.forEach(group, (radio) => radio.addEventListener('change', () => this.UpdateRadio(group)));
     });
 
     this.select[0].addEventListener('choice', () => {
@@ -35,10 +28,10 @@ class CalculatorController {
   }
 
   UpdateRadio(group) {
-    group.forEach((radio) => {
+    _.forEach(group, (radio) => {
       if (radio.checked) radio.parentElement.classList.add('calculator__value--active');
       else radio.parentElement.classList.remove('calculator__value--active');
-    });
+    })
   }
 
   UpdatePrice() {
--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,18 +1,26 @@
 const header = document.querySelector('#header');
 const headerDropdown = header.querySelectorAll('.nav__list .dropdown > .dropdown__header');
+const dropdownOpenClass = 'dropdown--open';
 
 let scrollPosition = window.pageYOffset;
 
-headerDropdown.forEach((dropdown) => {
-  dropdown.addEventListener('click', () => dropdown.parentNode.classList.toggle('dropdown--open'));
+_.forEach(headerDropdown, (dropdown) => {
+  dropdown.addEventListener('click', () => Dropdown(dropdown, 'toggle'));
   window.addEventListener('click',
     (e) => {
-      if (!dropdown.parentNode.contains(e.target)) dropdown.parentNode.classList.remove('dropdown--open');
+      if (!dropdown.parentNode.contains(e.target)) Dropdown(dropdown, 'hide');
     });
 
   window.addEventListener('scroll',
     () => {
       scrollPosition = window.pageYOffset;
-      if (scrollPosition >= 150) dropdown.parentNode.classList.remove('dropdown--open');
+      if (scrollPosition >= 150) Dropdown(dropdown, 'hide');
     });
-});
+})
+
+function Dropdown(dropdown, action) {
+  const className = 'dropdown--open';
+
+  if (action === 'hide') dropdown.parentNode.classList.remove(className);
+  else if (action === 'toggle') dropdown.parentNode.classList.toggle(className);
+}
