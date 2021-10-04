e5af1432dfd6982f45876fa4ab859fe1e8631bfd
Nikita <main.acr0matic@gmail.com>
2021-10-04 15:08:40 +0300

    feat: тултипы и доработка модального окна

--- src/scripts/init.js
+++ src/scripts/init.js
@@ -4,3 +4,11 @@ const modalParams = {
 };
 
 MicroModal.init(modalParams);
+
+
+tippy('.input__warning[data-tippy-content]', {
+  placement: 'right',
+  allowHTML: true,
+  maxWidth: 300,
+  theme: 'flat',
+});
