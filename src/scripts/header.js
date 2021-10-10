3a2cd00813c62e72187dc26daf8074bf295c874b
Nikita <main.acr0matic@gmail.com>
2021-10-10 23:46:21 +0300

    feat: адаптив под мобильные устройства

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -27,15 +27,16 @@ function Dropdown(dropdown, action) {
 
 /* Логика для мобильного меню */
 const mobileMenuButton = document.querySelector('.header__mobile');
+const mobileBurger = mobileMenuButton.querySelector('.hamburger')
 const mobileMenu = document.getElementById('mobile-menu');
 const mobileOverlay = mobileMenu.querySelector('.mobile-menu__overlay');
 
 mobileMenuButton.addEventListener('click', () => {
-  mobileMenuButton.classList.toggle('is-active');
+  mobileBurger.classList.toggle('is-active');
   mobileMenu.classList.toggle('mobile-menu--open');
 });
 
 mobileOverlay.addEventListener('click', () => {
-  mobileMenuButton.classList.remove('is-active');
+  mobileBurger.classList.remove('is-active');
   mobileMenu.classList.remove('mobile-menu--open');
 });
