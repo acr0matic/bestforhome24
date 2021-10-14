322ac50613265b0b268d856c04d5ab71a345f6cb
Nikita <main.acr0matic@gmail.com>
2021-10-14 14:29:23 +0300

    feat: главный экран страницы "москитки"

--- src/mosquito.php
+++ src/mosquito.php
@@ -1,3 +1,46 @@
 <?php include_once('partials/header.php'); ?>
 
+<main id="catalog">
+  <section id="hero">
+    <div class="hero">
+      <div class="hero__background">
+        <img src="img/page/mosquito/hero/banner.jpg" alt="" class="hero__image image image--cover">
+      </div>
+      <!-- /.hero__background -->
+
+      <div class="container">
+        <div class="hero__content">
+          <div class="section__header">
+            <h1 class="hero__title section__title page__title">Москитные сетки</h1>
+            <nav class="nav nav--section">
+              <ul class="nav__list">
+                <li class="nav__item">
+                  <a class="nav__link link link--nav" href="">Виды конструкций</a>
+                </li>
+
+                <li class="nav__item">
+                  <a class="nav__link link link--nav" href="">Виды полотна</a>
+                </li>
+
+                <li class="nav__item">
+                  <a class="nav__link link link--nav" href="">Калькулятор</a>
+                </li>
+              </ul>
+            </nav>
+            <!-- /.nav -->
+          </div>
+          <!-- /.section__header -->
+
+          <p class="hero__description section__description">
+            Наша компания изготавливает москитные сетки и рулонные шторы уже больше 10 лет. Каждая сетка или штора делается строго индивидуально под Ваши окна. В нашем каталоге тысячи цветов и вариаций рисунков для штор, а также, все возможные виды антимоскитного полотна для любой формы москитной сетки.
+          </p>
+        </div>
+        <!-- /.hero__content -->
+      </div>
+      <!-- /.container -->
+    </div>
+    <!-- /.hero -->
+  </section>
+</main>
+
 <?php include_once('partials/footer.php'); ?>
