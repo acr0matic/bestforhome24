b40fce49ea96ff78363c91cf28d6e38b19c31248
Nikita <main.acr0matic@gmail.com>
2021-10-29 19:38:55 +0300

    feat: правка отступов

--- src/mosquito-item.php
+++ src/mosquito-item.php
@@ -43,7 +43,7 @@
               </p>
 
               <div class="showcase__action">
-                <button class="button button-outline me-2">Заказать</button>
+                <button data-micromodal-trigger="modal-callback" class="button button-outline me-3">Заказать</button>
                 <button class="button button-primary">Калькулятор</button>
               </div>
               <!-- /.showcase__action -->
@@ -146,6 +146,236 @@
       <!-- /.showcase__row showcase__row--secondary -->
     </div>
     <!-- /.showcase -->
+  </section>
+
+  <section id="compatibility" class="section">
+    <div class="container">
+      <div class="compatibility">
+        <h2 class="section__title">Какое полотно подходит</h2>
+
+
+        <div class="material__slider position-relative">
+          <div class="swiper swiper--shadow slider-material">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide">
+                <div class="material__item">
+                  <div class="material__header">
+                    <div class="material__picture">
+                      <img src="img/catalog/mosquito/material/standart.svg" alt="" class="material__image image">
+                    </div>
+                    <!-- /.material__picture -->
+
+                    <div class="material__wrapper">
+                      <div class="material__head">
+                        <h3 class="material__name">
+                          <a href="" class="link link--nav">Стандарт</a>
+                        </h3>
+
+                        <a class="material__lightbox" data-fslightbox="id-1" href="img/catalog/mosquito/constuction/standart.svg">
+                          <img src="img/icons/misc/help.svg" alt="" class="material__help">
+                        </a>
+                      </div>
+                      <!-- /.material__head -->
+                      <span class="material__price">От 1 000р</span>
+
+                      <div class="material__params">
+                        <div class="material__param">Размер ячейки: <span class="material__value">1.1 х 1.1 мм.</span></div>
+                        <div class="material__param">Толщина нити: <span class="material__value">0.28 мм.</span></div>
+                      </div>
+                      <!-- /.material__params -->
+                    </div>
+                    <!-- /.material__wrapper -->
+                  </div>
+                  <!-- /.material__header -->
+
+                  <div class="material__content">
+                    <p class="material__description">
+                      Устойчиво к повышенной влажности и солнечному излучению.
+                    </p>
+                  </div>
+                  <!-- /.material__content -->
+
+                  <div class="material__action">
+                    <a href="" class="material__link link link--nav link--arrow">
+                      Подробнее
+                      <svg class="link__arrow svg ms-2" width="13" height="10" viewBox="0 0 10 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M6.83007 0.139209L9.86053 3.16392C10.0465 3.34953 10.0465 3.65047 9.86053 3.83608L6.83007 6.86079C6.64411 7.0464 6.3426 7.0464 6.15663 6.86079C5.97067 6.67518 5.97067 6.37424 6.15663 6.18863L8.37418 3.97529L0 3.97529L0 3.02471L8.37418 3.02471L6.15663 0.811367C5.97067 0.625755 5.97067 0.32482 6.15663 0.139209C6.3426 -0.0464029 6.64411 -0.0464029 6.83007 0.139209Z" fill="#969696"></path>
+                      </svg>
+                    </a>
+                  </div>
+                  <!-- /.material__action -->
+                </div>
+                <!-- /.material___item -->
+              </div>
+              <!-- /.swiper-slide -->
+
+              <div class="swiper-slide">
+                <div class="material__item">
+                  <div class="material__header">
+                    <div class="material__picture">
+                      <img src="img/catalog/mosquito/material/dust.svg" alt="" class="material__image image">
+                    </div>
+                    <!-- /.material__picture -->
+
+                    <div class="material__wrapper">
+                      <div class="material__head">
+                        <h3 class="material__name">
+                          <a href="" class="link link--nav">Антипыль</a>
+                        </h3>
+
+                        <a class="material__lightbox" data-fslightbox="id-1" href="img/catalog/mosquito/constuction/dust.svg">
+                          <img src="img/icons/misc/help.svg" alt="" class="material__help">
+                        </a>
+                      </div>
+                      <!-- /.material__head -->
+
+                      <span class="material__price">От 1 000р</span>
+
+                      <div class="material__params">
+                        <div class="material__param">Размер ячейки: <span class="material__value">1.25</span></div>
+                        <div class="material__param">Толщина нити: <span class="material__value">1.25x1.25</span></div>
+                      </div>
+                      <!-- /.material__params -->
+                    </div>
+                    <!-- /.material__wrapper -->
+                  </div>
+                  <!-- /.material__header -->
+
+                  <div class="material__content">
+                    <p class="material__description">
+                      Недорогое обычное полотно из стекловолокна с размером ячеек 1,2х1,2 мм. Подходит для разных систем, устойчиво к повышенной влажности и солнечному излучению.
+                    </p>
+                  </div>
+                  <!-- /.material__content -->
+
+                  <div class="material__action">
+                    <a href="" class="material__link link link--nav link--arrow">
+                      Подробнее
+                      <svg class="link__arrow svg ms-2" width="13" height="10" viewBox="0 0 10 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M6.83007 0.139209L9.86053 3.16392C10.0465 3.34953 10.0465 3.65047 9.86053 3.83608L6.83007 6.86079C6.64411 7.0464 6.3426 7.0464 6.15663 6.86079C5.97067 6.67518 5.97067 6.37424 6.15663 6.18863L8.37418 3.97529L0 3.97529L0 3.02471L8.37418 3.02471L6.15663 0.811367C5.97067 0.625755 5.97067 0.32482 6.15663 0.139209C6.3426 -0.0464029 6.64411 -0.0464029 6.83007 0.139209Z" fill="#969696"></path>
+                      </svg>
+                    </a>
+                  </div>
+                  <!-- /.material__action -->
+                </div>
+                <!-- /.material___item -->
+              </div>
+              <!-- /.swiper-slide -->
+
+              <div class="swiper-slide">
+                <div class="material__item">
+                  <div class="material__header">
+                    <div class="material__picture">
+                      <img src="img/catalog/mosquito/material/pollen.svg" alt="" class="material__image image">
+                    </div>
+                    <!-- /.material__picture -->
+
+                    <div class="material__wrapper">
+                      <div class="material__head">
+                        <h3 class="material__name">
+                          <a href="" class="link link--nav">Антипыльца</a>
+                        </h3>
+
+                        <a class="material__lightbox" data-fslightbox="id-1" href="img/catalog/mosquito/constuction/pollen.svg">
+                          <img src="img/icons/misc/help.svg" alt="" class="material__help">
+                        </a>
+                      </div>
+                      <!-- /.material__head -->
+
+                      <span class="material__price">От 1 000р</span>
+
+                      <div class="material__params">
+                        <div class="material__param">Размер ячейки: <span class="material__value">1.25</span></div>
+                        <div class="material__param">Толщина нити: <span class="material__value">1.25x1.25</span></div>
+                      </div>
+                      <!-- /.material__params -->
+                    </div>
+                    <!-- /.material__wrapper -->
+                  </div>
+                  <!-- /.material__header -->
+
+                  <div class="material__content">
+                    <p class="material__description">
+                      Рекомендована для нижних этажей многоэтажек. Ячейки 0,7х1 мм для защиты как от насекомых, так и частиц крупной, мелкой пыли. Устанавливается на окнах и дверях балконов, террас, частных домов.
+                    </p>
+                  </div>
+                  <!-- /.material__content -->
+
+                  <div class="material__action">
+                    <a href="" class="material__link link link--nav link--arrow">
+                      Подробнее
+                      <svg class="link__arrow svg ms-2" width="13" height="10" viewBox="0 0 10 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M6.83007 0.139209L9.86053 3.16392C10.0465 3.34953 10.0465 3.65047 9.86053 3.83608L6.83007 6.86079C6.64411 7.0464 6.3426 7.0464 6.15663 6.86079C5.97067 6.67518 5.97067 6.37424 6.15663 6.18863L8.37418 3.97529L0 3.97529L0 3.02471L8.37418 3.02471L6.15663 0.811367C5.97067 0.625755 5.97067 0.32482 6.15663 0.139209C6.3426 -0.0464029 6.64411 -0.0464029 6.83007 0.139209Z" fill="#969696"></path>
+                      </svg>
+                    </a>
+                  </div>
+                  <!-- /.material__action -->
+                </div>
+                <!-- /.material___item -->
+              </div>
+              <!-- /.swiper-slide -->
+
+              <div class="swiper-slide">
+                <div class="material__item">
+                  <div class="material__header">
+                    <div class="material__picture">
+                      <img src="img/catalog/mosquito/material/standart.svg" alt="" class="material__image image">
+                    </div>
+                    <!-- /.material__picture -->
+
+                    <div class="material__wrapper">
+                      <h3 class="material__name">
+                        <a href="" class="link link--nav">Стандарт</a>
+                      </h3>
+
+                      <span class="material__price">От 1 000р</span>
+
+                      <div class="material__params">
+                        <div class="material__param">Размер ячейки: <span class="material__value">1.25</span></div>
+                        <div class="material__param">Толщина нити: <span class="material__value">1.25x1.25</span></div>
+                      </div>
+                      <!-- /.material__params -->
+                    </div>
+                    <!-- /.material__wrapper -->
+                  </div>
+                  <!-- /.material__header -->
+
+                  <div class="material__content">
+                    <p class="material__description">
+                      Недорогое обычное полотно из стекловолокна с размером ячеек 1,2х1,2 мм. Подходит для разных систем, устойчиво к повышенной влажности и солнечному излучению.
+                    </p>
+                  </div>
+                  <!-- /.material__content -->
+
+                  <div class="material__action">
+                    <a href="" class="material__link link link--nav link--arrow">
+                      Подробнее
+                      <svg class="link__arrow svg ms-2" width="13" height="10" viewBox="0 0 10 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M6.83007 0.139209L9.86053 3.16392C10.0465 3.34953 10.0465 3.65047 9.86053 3.83608L6.83007 6.86079C6.64411 7.0464 6.3426 7.0464 6.15663 6.86079C5.97067 6.67518 5.97067 6.37424 6.15663 6.18863L8.37418 3.97529L0 3.97529L0 3.02471L8.37418 3.02471L6.15663 0.811367C5.97067 0.625755 5.97067 0.32482 6.15663 0.139209C6.3426 -0.0464029 6.64411 -0.0464029 6.83007 0.139209Z" fill="#969696"></path>
+                      </svg>
+                    </a>
+                  </div>
+                  <!-- /.material__action -->
+                </div>
+                <!-- /.material___item -->
+              </div>
+              <!-- /.swiper-slide -->
+            </div>
+            <!-- /.swiper-wrapper -->
+          </div>
+          <!-- /.swiper constuction-slider -->
+
+          <div class="swiper__control">
+            <div class="swiper-button-prev slider-material__prev"></div>
+            <div class="swiper-button-next slider-material__next"></div>
+          </div>
+          <!-- /.swiper__control -->
+        </div>
+        <!-- /.constuction__slider -->
+      </div>
+      <!-- /.compatibility -->
+    </div>
+    <!-- /.container -->
   </section>
 
   <?php include_once('partials/calculator.php'); ?>
