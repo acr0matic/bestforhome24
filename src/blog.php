d11b5b1d91a0ab31281555763e7b35302a8b83bc
Nikita <main.acr0matic@gmail.com>
2021-10-29 13:16:39 +0300

    feat: страница блога

---
+++ src/blog.php
@@ -0,0 +1,108 @@
+<?php include_once('partials/header.php'); ?>
+
+<main id="blog">
+  <section id="blog" class="section">
+    <div class="container">
+      <h2 class="section__title">Полезные статьи</h2>
+
+      <div class="blog">
+        <div class="row">
+          <div class="col-12 col-md-4 mb-4 mb-md-0">
+            <div class="blog__card blog-card blog-card--main">
+              <div class="blog-card__picture">
+                <img src="img/page/main/useful/1.jpg" alt="" class="blog-card__image image image--cover">
+              </div>
+              <!-- /.blog-card__picture -->
+
+              <div class="blog-card__content">
+                <h3 class="blog-card__title">Как установить москитную сетку</h3>
+                <p class="blog-card__description">Далеко-далеко за словесными, горами в стране гласных и согласных живут рыбные тексты. Точках маленькая скатился речью, то домах языком несколько пор продолжил обеспечивает если за, страну агентство подпоясал мир строчка рыбными вдали решила заглавных текстами текстов ipsum они жаренные бросил дорогу. Они?</p>
+                <div class="blog-card__action">
+                  <button class="blog-card__button button button-primary">Читать</button>
+                </div>
+                <!-- /.blog-card__action -->
+              </div>
+              <!-- /.blog-card__content -->
+            </div>
+            <!-- /.blog__card blog-card blog-card--main -->
+          </div>
+          <!-- /.col-3 -->
+
+          <div class="col-12 col-md-8">
+            <div class="blog__card blog-card mb-4 mb-md-0">
+              <div class="blog-card__picture">
+                <img src="img/page/main/useful/1.jpg" alt="" class="blog-card__image image image--cover">
+              </div>
+              <!-- /.blog-card__picture -->
+
+              <div class="blog-card__content">
+                <h3 class="blog-card__title">Как установить москитную сетку</h3>
+                <p class="blog-card__description">Далеко-далеко за словесными, горами в стране гласных и согласных живут рыбные тексты. Точках маленькая скатился речью, то домах языком несколько пор продолжил обеспечивает если за, страну агентство подпоясал мир строчка рыбными вдали решила заглавных текстами текстов ipsum они жаренные бросил дорогу. Они?</p>
+                <div class="blog-card__action">
+                  <button class="blog-card__button button button-primary">Читать</button>
+                </div>
+                <!-- /.blog-card__action -->
+              </div>
+              <!-- /.blog-card__content -->
+            </div>
+            <!-- /.blog__card blog-card -->
+
+            <div class="blog__card blog-card mb-4 mb-md-0">
+              <div class="blog-card__picture">
+                <img src="img/page/main/useful/1.jpg" alt="" class="blog-card__image image image--cover">
+              </div>
+              <!-- /.blog-card__picture -->
+
+              <div class="blog-card__content">
+                <h3 class="blog-card__title">Как установить москитную сетку</h3>
+                <p class="blog-card__description">Далеко-далеко за словесными, горами в стране гласных и согласных живут рыбные тексты. Точках маленькая скатился речью, то домах языком несколько пор продолжил обеспечивает если за, страну агентство подпоясал мир строчка рыбными вдали решила заглавных текстами текстов ipsum они жаренные бросил дорогу. Они?</p>
+                <div class="blog-card__action">
+                  <button class="blog-card__button button button-primary">Читать</button>
+                </div>
+                <!-- /.blog-card__action -->
+              </div>
+              <!-- /.blog-card__content -->
+            </div>
+            <!-- /.blog__card blog-card -->
+
+            <div class="blog__card blog-card mb-4 mb-md-0">
+              <div class="blog-card__picture">
+                <img src="img/page/main/useful/1.jpg" alt="" class="blog-card__image image image--cover">
+              </div>
+              <!-- /.blog-card__picture -->
+
+              <div class="blog-card__content">
+                <h3 class="blog-card__title">Как установить москитную сетку</h3>
+                <p class="blog-card__description">Далеко-далеко за словесными, горами в стране гласных и согласных живут рыбные тексты. Точках маленькая скатился речью, то домах языком несколько пор продолжил обеспечивает если за, страну агентство подпоясал мир строчка рыбными вдали решила заглавных текстами текстов ipsum они жаренные бросил дорогу. Они?</p>
+                <div class="blog-card__action">
+                  <button class="blog-card__button button button-primary">Читать</button>
+                </div>
+                <!-- /.blog-card__action -->
+              </div>
+              <!-- /.blog-card__content -->
+            </div>
+            <!-- /.blog__card blog-card -->
+          </div>
+          <!-- /.col-9 -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.blog -->
+    </div>
+    <!-- /.container -->
+  </section>
+
+  <script data-b24-form="click/1/cx2vzw" data-skip-moving="true">
+    (function(w, d, u) {
+      var s = d.createElement('script');
+      s.async = true;
+      s.src = u + '?' + (Date.now() / 180000 | 0);
+      var h = d.getElementsByTagName('script')[0];
+      h.parentNode.insertBefore(s, h);
+    })(window, document, 'https://cdn-ru.bitrix24.ru/b11724294/crm/form/loader_1.js');
+  </script>
+
+  <?php include_once('partials/callback.php'); ?>
+</main>
+
+<?php include_once('partials/footer.php'); ?>
