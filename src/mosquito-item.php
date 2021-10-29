29f24df90aefb4bd9cbcd2b6764b4fb79a3732dd
Nikita <main.acr0matic@gmail.com>
2021-10-29 19:06:33 +0300

    feat: преимущества

--- src/mosquito-item.php
+++ src/mosquito-item.php
@@ -2,59 +2,155 @@
 
 <main id="item">
   <section class="section" id="showcase">
-    <div class="container">
-      <div class="showcase">
-        <div class="row">
-          <div class="col-5">
-            <div class="slider">
-              <div class="swiper slider-gallery">
-                <div class="swiper-wrapper">
-                  <div class="swiper-slide">
-                    <div class="showcase__picture">
-                      <img src="img/catalog/mosquito/gallery/1.jpg" alt="" class="showcase__image image image--cover">
+    <div class="showcase">
+      <div class="showcase__row">
+        <div class="container">
+          <div class="row">
+            <div class="col-5">
+              <div class="slider">
+                <div class="swiper slider-gallery">
+                  <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                      <div class="showcase__picture">
+                        <img src="img/catalog/mosquito/gallery/1.jpg" alt="" class="showcase__image image image--cover">
+                      </div>
+                      <!-- /.showcase__picture -->
                     </div>
-                    <!-- /.showcase__picture -->
-                  </div>
-                  <!-- /.swiper-slide -->
+                    <!-- /.swiper-slide -->
 
-                  <div class="swiper-slide">
-                    <div class="showcase__picture">
-                      <img src="img/catalog/mosquito/gallery/1.jpg" alt="" class="showcase__image image image--cover">
+                    <div class="swiper-slide">
+                      <div class="showcase__picture">
+                        <img src="img/catalog/mosquito/gallery/1.jpg" alt="" class="showcase__image image image--cover">
+                      </div>
+                      <!-- /.showcase__picture -->
                     </div>
-                    <!-- /.showcase__picture -->
+                    <!-- /.swiper-slide -->
                   </div>
-                  <!-- /.swiper-slide -->
+                  <!-- /.swiper-wrapper -->
                 </div>
-                <!-- /.swiper-wrapper -->
+                <!-- /.swiper slider-gallery -->
               </div>
-              <!-- /.swiper slider-gallery -->
+              <!-- /.slider -->
             </div>
-            <!-- /.slider -->
-          </div>
-          <!-- /.col-6 -->
+            <!-- /.col-6 -->
 
-          <div class="col-7">
-            <h1 class="showcase__title section__title page__title"><span>РАМОЧНЫЕ</span> МОСКИТНЫЕ СЕТКИ</h1>
-            <p class="showcase__description custom-scrollbar">
-              Рамочная москитная сетка — удобная в эксплуатации конструкция. Мы делаем рамочные москитные сетки на окна любых размеров. В «Оконном сервисе» можно заказать «москитку» в рамке на комнатные оконные рамы, на балкон, эркер и другие.
-              <br> <br>
-              Рамочная москитная сетка — удобная в эксплуатации конструкция. При необходимости вы сможете ее без проблем снять, чтобы почистить от насекомых или помыть. Мы делаем рамочные москитные сетки на окна любых размеров. В «Оконном сервисе» можно заказать «москитку» в рамке на комнатные оконные рамы, на балкон, эркер и другие.
-            </p>
+            <div class="col-7 p-3">
+              <h1 class="showcase__title section__title page__title"><span>РАМОЧНЫЕ</span> МОСКИТНЫЕ СЕТКИ</h1>
+              <p class="showcase__description custom-scrollbar">
+                Рамочная москитная сетка — удобная в эксплуатации конструкция. Мы делаем рамочные москитные сетки на окна любых размеров. В «Оконном сервисе» можно заказать «москитку» в рамке на комнатные оконные рамы, на балкон, эркер и другие.
+                <br> <br>
+                Рамочная москитная сетка — удобная в эксплуатации конструкция. При необходимости вы сможете ее без проблем снять, чтобы почистить от насекомых или помыть. Мы делаем рамочные москитные сетки на окна любых размеров. В «Оконном сервисе» можно заказать «москитку» в рамке на комнатные оконные рамы, на балкон, эркер и другие.
+              </p>
 
-            <div class="showcase__action">
-              <button class="button button-outline me-2">Заказать</button>
-              <button class="button button-primary">Калькулятор</button>
+              <div class="showcase__action">
+                <button class="button button-outline me-2">Заказать</button>
+                <button class="button button-primary">Калькулятор</button>
+              </div>
+              <!-- /.showcase__action -->
             </div>
-            <!-- /.showcase__action -->
+            <!-- /.col-6 -->
           </div>
-          <!-- /.col-6 -->
+          <!-- /.row -->
         </div>
-        <!-- /.row -->
+        <!-- /.container -->
       </div>
-      <!-- /.showcase -->
+      <!-- /.showcase__row -->
+
+      <div class="showcase__row showcase__row--secondary">
+        <div class="container">
+          <div class="row">
+            <div class="col-4">
+              <div class="showcase__advantage showcase-advantage">
+                <img class="showcase-advantage__icon" src="img/icons/icon.svg" alt="">
+
+                <div class="showcase-advantage__wrapper">
+                  <h3 class="showcase-advantage__title">Простота в установке</h3>
+                  <p class="showcase-advantage__description">
+                    Легко установить и демонтировать благодаря удобным ручкам.
+                  </p>
+                </div>
+                <!-- /.showcase-advantage__wrapper -->
+              </div>
+              <!-- /.showcase__advantage showcase-advantage -->
+            </div>
+            <!-- /.col-4 -->
+
+            <div class="col-4">
+              <div class="showcase__advantage showcase-advantage">
+                <img class="showcase-advantage__icon" src="img/icons/icon.svg" alt="">
+
+                <div class="showcase-advantage__wrapper">
+                  <h3 class="showcase-advantage__title">Доступная стоимость</h3>
+                  <p class="showcase-advantage__description">
+                    Рамочные конструкции являются надежным и бюджетным вариантом сеток от комаров.
+                  </p>
+                </div>
+                <!-- /.showcase-advantage__wrapper -->
+              </div>
+              <!-- /.showcase__advantage showcase-advantage -->
+            </div>
+            <!-- /.col-4 -->
+
+            <div class="col-4">
+              <div class="showcase__advantage showcase-advantage">
+                <img class="showcase-advantage__icon" src="img/icons/icon.svg" alt="">
+
+                <div class="showcase-advantage__wrapper">
+                  <h3 class="showcase-advantage__title">Простая установка</h3>
+                  <p class="showcase-advantage__description">
+                    Монтаж без сверления, шурупов, клея, отверток и монтажников.
+                  </p>
+                </div>
+                <!-- /.showcase-advantage__wrapper -->
+              </div>
+              <!-- /.showcase__advantage showcase-advantage -->
+            </div>
+            <!-- /.col-4 -->
+
+            <div class="col-4 mt-5">
+              <div class="showcase__advantage showcase-advantage">
+                <img class="showcase-advantage__icon" src="img/icons/icon.svg" alt="">
+
+                <div class="showcase-advantage__wrapper">
+                  <h3 class="showcase-advantage__title">Доступная стоимость</h3>
+                  <p class="showcase-advantage__description">
+                    Рамочные конструкции являются надежным и бюджетным вариантом сеток от комаров.
+                  </p>
+                </div>
+                <!-- /.showcase-advantage__wrapper -->
+              </div>
+              <!-- /.showcase__advantage showcase-advantage -->
+            </div>
+            <!-- /.col-4 -->
+
+            <div class="col-4 mt-5">
+              <div class="showcase__advantage showcase-advantage">
+                <img class="showcase-advantage__icon" src="img/icons/icon.svg" alt="">
+
+                <div class="showcase-advantage__wrapper">
+                  <h3 class="showcase-advantage__title">Простая установка</h3>
+                  <p class="showcase-advantage__description">
+                    Монтаж без сверления, шурупов, клея, отверток и монтажников.
+                  </p>
+                </div>
+                <!-- /.showcase-advantage__wrapper -->
+              </div>
+              <!-- /.showcase__advantage showcase-advantage -->
+            </div>
+            <!-- /.col-4 -->
+          </div>
+          <!-- /.row -->
+        </div>
+        <!-- /.container -->
+      </div>
+      <!-- /.showcase__row showcase__row--secondary -->
     </div>
-    <!-- /.container -->
+    <!-- /.showcase -->
   </section>
+
+  <?php include_once('partials/calculator.php'); ?>
+
+  <?php include_once('partials/order.php'); ?>
 
   <?php include_once('partials/callback.php'); ?>
 </main>
