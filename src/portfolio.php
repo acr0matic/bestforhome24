202f6c58f8837c9520b9a00454888343719426af
Nikita <main.acr0matic@gmail.com>
2021-11-04 17:33:19 +0300

    feat: страница портфолио

---
+++ src/portfolio.php
@@ -0,0 +1,141 @@
+<?php include_once('partials/header.php'); ?>
+
+<main id="portfolio">
+  <section class="section" id="portfolio">
+    <div class="container">
+      <div class="section__header">
+        <h2 class="section__title">Наши работы</h2>
+        <span class="link link--arrow">
+          Более 500 довольных клиентов
+        </span>
+      </div>
+      <!-- /.section__header -->
+
+      <div class="portfolio">
+        <div class="row">
+          <div class="col-12 col-md-3 mb-4 mb-md-0">
+            <div class="portfolio__menu">
+              <div class="portfolio__wrapper custom-scrollbar">
+                <div class="portfolio__category">Москитные сетки</div>
+                <ul class="portfolio__list">
+                  <li class="portfolio__subcategory portfolio__highlight">Стандарт</li>
+                  <li class="portfolio__subcategory">Внутренняя</li>
+                  <li class="portfolio__subcategory">Антипыльца</li>
+                  <li class="portfolio__subcategory">Антипыль</li>
+                  <li class="portfolio__subcategory">Ультравью</li>
+                  <li class="portfolio__subcategory">Проведал</li>
+                  <li class="portfolio__subcategory">Дверь</li>
+                  <li class="portfolio__subcategory mb-2">Антикошка</li>
+                </ul>
+
+                <div class="portfolio__category">Рулонные шторы</div>
+                <ul class="portfolio__list">
+                  <li class="portfolio__subcategory">Жалюзи</li>
+                  <li class="portfolio__subcategory">День-Ночь</li>
+                  <li class="portfolio__subcategory">Плиссе</li>
+                  <li class="portfolio__subcategory">Гранд</li>
+                  <li class="portfolio__subcategory">Стандарт</li>
+                  <li class="portfolio__subcategory">Гранд День-Ночь</li>
+                </ul>
+
+                <div class="portfolio__category">Тонировка окон</div>
+                <div class="portfolio__category">Прочие услуги</div>
+              </div>
+              <!-- /.portfolio__wrapper -->
+            </div>
+            <!-- /.portfolio__menu -->
+          </div>
+          <!-- /.col-4 -->
+
+          <div class="col-12 col-md-9">
+            <div class="portfolio__gallery">
+              <div class="row">
+                <div class="col-6 col-md-4 mb-4">
+                  <div class="portfolio__picture position-relative">
+                    <img src="img/misc/preloader.svg" class="preloader" alt="">
+                    <img data-src="img/portfolio/1.jpg" alt="" class="portfolio__image image image--cover lazy">
+                  </div>
+                  <!-- /.portfolio__picture -->
+                </div>
+                <!-- /.col-4 -->
+
+                <div class="col-6 col-md-4 mb-4">
+                  <div class="portfolio__picture position-relative">
+                    <img src="img/misc/preloader.svg" class="preloader" alt="">
+                    <img data-src="img/portfolio/2.jpg" alt="" class="portfolio__image image image--cover lazy">
+                  </div>
+                  <!-- /.portfolio__picture -->
+                </div>
+                <!-- /.col-4 -->
+
+                <div class="col-6 col-md-4 mb-4">
+                  <div class="portfolio__picture position-relative">
+                    <img src="img/misc/preloader.svg" class="preloader" alt="">
+                    <img data-src="img/portfolio/3.jpg" alt="" class="portfolio__image image image--cover lazy">
+                  </div>
+                  <!-- /.portfolio__picture -->
+                </div>
+                <!-- /.col-4 -->
+
+                <div class="col-6 col-md-4 mb-4">
+                  <div class="portfolio__picture position-relative">
+                    <img src="img/misc/preloader.svg" class="preloader" alt="">
+                    <img data-src="img/portfolio/1.jpg" alt="" class="portfolio__image image image--cover lazy">
+                  </div>
+                  <!-- /.portfolio__picture -->
+                </div>
+                <!-- /.col-4 -->
+
+                <div class="col-6 col-md-4 mb-4">
+                  <div class="portfolio__picture position-relative">
+                    <img src="img/misc/preloader.svg" class="preloader" alt="">
+                    <img data-src="img/portfolio/3.jpg" alt="" class="portfolio__image image image--cover lazy">
+                  </div>
+                  <!-- /.portfolio__picture -->
+                </div>
+                <!-- /.col-4 -->
+
+                <div class="col-6 col-md-4 mb-4">
+                  <div class="portfolio__picture position-relative">
+                    <img src="img/misc/preloader.svg" class="preloader" alt="">
+                    <img data-src="img/portfolio/2.jpg" alt="" class="portfolio__image image image--cover lazy">
+                  </div>
+                  <!-- /.portfolio__picture -->
+                </div>
+                <!-- /.col-4 -->
+
+                <div class="col-6 col-md-4">
+                  <div class="portfolio__picture position-relative">
+                    <img src="img/misc/preloader.svg" class="preloader" alt="">
+                    <img data-src="img/portfolio/1.jpg" alt="" class="portfolio__image image image--cover lazy">
+                  </div>
+                  <!-- /.portfolio__picture -->
+                </div>
+                <!-- /.col-4 -->
+
+                <div class="col-6 col-md-4">
+                  <div class="portfolio__picture position-relative">
+                    <img src="img/misc/preloader.svg" class="preloader" alt="">
+                    <img data-src="img/portfolio/1.jpg" alt="" class="portfolio__image image image--cover lazy">
+                  </div>
+                  <!-- /.portfolio__picture -->
+                </div>
+                <!-- /.col-4 -->
+              </div>
+              <!-- /.row -->
+            </div>
+            <!-- /.portfolio__gallery -->
+          </div>
+          <!-- /.col-8 -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.portfolio -->
+    </div>
+    <!-- /.container -->
+  </section>
+
+  <?php include_once('partials/callback.php'); ?>
+</main>
+
+<?php include_once('partials/footer.php'); ?>
