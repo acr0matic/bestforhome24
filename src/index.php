e5398011dc07af4093900aee446526b85eee5e12
Nikita <main.acr0matic@gmail.com>
2021-11-05 13:00:21 +0300

    feat: обновлен блок портфолио на главной

--- src/index.php
+++ src/index.php
@@ -394,96 +394,77 @@
 
       <div class="portfolio">
         <div class="row">
-          <div class="col-3">
-            <div class="portfolio__menu">
-              <div class="portfolio__wrapper custom-scrollbar">
-                <div class="portfolio__category">Москитные сетки</div>
-                <ul class="portfolio__list">
-                  <li class="portfolio__subcategory portfolio__highlight">Стандарт</li>
-                  <li class="portfolio__subcategory">Внутренняя</li>
-                  <li class="portfolio__subcategory">Антипыльца</li>
-                  <li class="portfolio__subcategory">Антипыль</li>
-                  <li class="portfolio__subcategory">Ультравью</li>
-                  <li class="portfolio__subcategory">Проведал</li>
-                  <li class="portfolio__subcategory">Дверь</li>
-                  <li class="portfolio__subcategory mb-2">Антикошка</li>
-                </ul>
-
-                <div class="portfolio__category">Рулонные шторы</div>
-                <ul class="portfolio__list">
-                  <li class="portfolio__subcategory">Жалюзи</li>
-                  <li class="portfolio__subcategory">День-Ночь</li>
-                  <li class="portfolio__subcategory">Плиссе</li>
-                  <li class="portfolio__subcategory">Гранд</li>
-                  <li class="portfolio__subcategory">Стандарт</li>
-                  <li class="portfolio__subcategory">Гранд День-Ночь</li>
-                </ul>
-
-                <div class="portfolio__category">Тонировка окон</div>
-                <div class="portfolio__category">Прочие услуги</div>
-              </div>
-              <!-- /.portfolio__wrapper -->
+          <div class="col-6 col-md-3 mb-4">
+            <div class="portfolio__picture position-relative">
+              <img src="img/misc/preloader.svg" class="preloader" alt="">
+              <img data-src="img/portfolio/3.jpg" alt="" class="portfolio__image image image--cover lazy">
             </div>
-            <!-- /.portfolio__menu -->
+            <!-- /.portfolio__picture -->
           </div>
-          <!-- /.col-4 -->
+          <!-- /.col-3 -->
 
-          <div class="col-9">
-            <div class="portfolio__gallery">
-              <div class="row">
-                <div class="col-4 mb-4">
-                  <div class="portfolio__picture">
-                    <img src="img/portfolio/1.jpg" alt="" class="portfolio__image image image--cover">
-                  </div>
-                  <!-- /.portfolio__picture -->
-                </div>
-                <!-- /.col-4 -->
-
-                <div class="col-4 mb-4">
-                  <div class="portfolio__picture">
-                    <img src="img/portfolio/2.jpg" alt="" class="portfolio__image image image--cover">
-                  </div>
-                  <!-- /.portfolio__picture -->
-                </div>
-                <!-- /.col-4 -->
-
-                <div class="col-4 mb-4">
-                  <div class="portfolio__picture">
-                    <img src="img/portfolio/3.jpg" alt="" class="portfolio__image image image--cover">
-                  </div>
-                  <!-- /.portfolio__picture -->
-                </div>
-                <!-- /.col-4 -->
-
-                <div class="col-4">
-                  <div class="portfolio__picture">
-                    <img src="img/portfolio/4.jpg" alt="" class="portfolio__image image image--cover">
-                  </div>
-                  <!-- /.portfolio__picture -->
-                </div>
-                <!-- /.col-4 -->
-
-                <div class="col-4">
-                  <div class="portfolio__picture">
-                    <img src="img/portfolio/5.jpg" alt="" class="portfolio__image image image--cover">
-                  </div>
-                  <!-- /.portfolio__picture -->
-                </div>
-                <!-- /.col-4 -->
-
-                <div class="col-4">
-                  <div class="portfolio__picture">
-                    <img src="img/portfolio/6.jpg" alt="" class="portfolio__image image image--cover">
-                  </div>
-                  <!-- /.portfolio__picture -->
-                </div>
-                <!-- /.col-4 -->
-              </div>
-              <!-- /.row -->
+          <div class="col-6 col-md-3 mb-4">
+            <div class="portfolio__picture position-relative">
+              <img src="img/misc/preloader.svg" class="preloader" alt="">
+              <img data-src="img/portfolio/3.jpg" alt="" class="portfolio__image image image--cover lazy">
             </div>
-            <!-- /.portfolio__gallery -->
+            <!-- /.portfolio__picture -->
           </div>
-          <!-- /.col-8 -->
+          <!-- /.col-3 -->
+
+          <div class="col-6 col-md-3 mb-4">
+            <div class="portfolio__picture position-relative">
+              <img src="img/misc/preloader.svg" class="preloader" alt="">
+              <img data-src="img/portfolio/3.jpg" alt="" class="portfolio__image image image--cover lazy">
+            </div>
+            <!-- /.portfolio__picture -->
+          </div>
+          <!-- /.col-3 -->
+
+          <div class="col-6 col-md-3 mb-4">
+            <div class="portfolio__picture position-relative">
+              <img src="img/misc/preloader.svg" class="preloader" alt="">
+              <img data-src="img/portfolio/3.jpg" alt="" class="portfolio__image image image--cover lazy">
+            </div>
+            <!-- /.portfolio__picture -->
+          </div>
+          <!-- /.col-3 -->
+
+          <div class="col-6 col-md-3 mb-4 md-0">
+            <div class="portfolio__picture position-relative">
+              <img src="img/misc/preloader.svg" class="preloader" alt="">
+              <img data-src="img/portfolio/3.jpg" alt="" class="portfolio__image image image--cover lazy">
+            </div>
+            <!-- /.portfolio__picture -->
+          </div>
+          <!-- /.col-3 -->
+
+          <div class="col-6 col-md-3 mb-4 md-0">
+            <div class="portfolio__picture position-relative">
+              <img src="img/misc/preloader.svg" class="preloader" alt="">
+              <img data-src="img/portfolio/3.jpg" alt="" class="portfolio__image image image--cover lazy">
+            </div>
+            <!-- /.portfolio__picture -->
+          </div>
+          <!-- /.col-3 -->
+
+          <div class="col-6 col-md-3">
+            <div class="portfolio__picture position-relative">
+              <img src="img/misc/preloader.svg" class="preloader" alt="">
+              <img data-src="img/portfolio/3.jpg" alt="" class="portfolio__image image image--cover lazy">
+            </div>
+            <!-- /.portfolio__picture -->
+          </div>
+          <!-- /.col-3 -->
+
+          <div class="col-6 col-md-3">
+            <div class="portfolio__picture position-relative">
+              <img src="img/misc/preloader.svg" class="preloader" alt="">
+              <img data-src="img/portfolio/3.jpg" alt="" class="portfolio__image image image--cover lazy">
+            </div>
+            <!-- /.portfolio__picture -->
+          </div>
+          <!-- /.col-3 -->
         </div>
         <!-- /.row -->
       </div>
