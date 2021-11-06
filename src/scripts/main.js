0c904acda8ee4b80c50c029994806dd09f6d8501
Nikita <main.acr0matic@gmail.com>
2021-11-06 22:36:09 +0300

    feat: прелоадер страницы

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,3 +5,10 @@ const Calculator = document.getElementById('calculator');
 if (Calculator) new CalculatorController(Calculator);
 
 calculator.updateAutoHeight();
+
+const preloader = document.getElementById('page-loader');
+document.addEventListener("DOMContentLoaded", function () {
+  window.setTimeout(function () {
+    preloader.classList.add('page-loader--hide');
+  }, 500);
+});
