c0c628485ce23ffafd447ef5c397fc6d2ab16a73
Nikita <main.acr0matic@gmail.com>
2022-01-19 19:39:00 +0300

    feat: верстка статьи

---
+++ src/article.php
@@ -0,0 +1,170 @@
+<?php include_once('partials/header.php') ?>
+
+<!-- Основной контент -->
+<main>
+  <section id="article" class="section">
+    <div class="container">
+      <div class="article">
+        <div class="article__header">
+          <a href="" class="card__link link link--nav link--arrow">Вернуться к статьям
+            <svg class="link__arrow svg ms-2" width="13" height="10" viewBox="0 0 10 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M6.83007 0.139209L9.86053 3.16392C10.0465 3.34953 10.0465 3.65047 9.86053 3.83608L6.83007 6.86079C6.64411 7.0464 6.3426 7.0464 6.15663 6.86079C5.97067 6.67518 5.97067 6.37424 6.15663 6.18863L8.37418 3.97529L0 3.97529L0 3.02471L8.37418 3.02471L6.15663 0.811367C5.97067 0.625755 5.97067 0.32482 6.15663 0.139209C6.3426 -0.0464029 6.64411 -0.0464029 6.83007 0.139209Z" fill="#969696"></path>
+            </svg>
+          </a>
+          <h1 class="article__title page__title section__title">
+            Как правильно делать замер окна
+          </h1>
+        </div>
+        <!-- /.article__header -->
+
+        <div class="article__body">
+          <h2>Далеко-далеко, за словесными.</h2>
+          <p>Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Предупреждал заголовок скатился диких путь мир рыбного жизни великий? Речью последний переулка переписывается, которое он которой текст сих они пояс снова то инициал буквенных буквоград свою гор. Там, использовало диких? Ты напоивший использовало океана собрал? Безорфографичный взобравшись, предупреждал текстами дал рукопись послушавшись над переписали текста, собрал если мир! Страна страну курсивных lorem одна запятых но бросил даль имени безопасную взгляд. Текстами пунктуация парадигматическая рукописи путь деревни скатился образ всеми, агентство напоивший одна диких текстов свое предупредила снова, злых залетают курсивных пояс всемогущая дал, себя жаренные точках несколько ему. Выйти, все?</p>
+          <br>
+          <img src="img/page/mosquito/hero/banner.jpg" alt="">
+          <br>
+          <ul>
+            <li>Lorem ipsum dolor sit amet consectetur.</li>
+            <li>Lorem ipsum dolor sit amet consectetur.</li>
+            <li>Lorem ipsum dolor sit amet consectetur.</li>
+            <li>Lorem ipsum dolor sit amet consectetur.</li>
+          </ul>
+          <br>
+          <h3>Далеко-далеко, за словесными.</h3>
+          <p>Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Океана предупредила пор текст текстами напоивший жаренные текстов сбить до возвращайся? Несколько скатился ты продолжил она залетают выйти ведущими одна использовало, щеке если рекламных lorem возвращайся правилами это запятых. Там!</p>
+          <br>
+          <ol>
+            <li>Lorem ipsum dolor sit amet consectetur.</li>
+            <li>Lorem ipsum dolor sit amet consectetur.</li>
+            <li>Lorem ipsum dolor sit amet consectetur.</li>
+            <li>Lorem ipsum dolor sit amet consectetur.</li>
+          </ol>
+          <br>
+          <br>
+          <h2>Далеко-далеко, за словесными.</h2>
+          <p>Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Предупреждал заголовок скатился диких путь мир рыбного жизни великий? Речью последний переулка переписывается, которое он которой текст сих они пояс снова то инициал буквенных буквоград свою гор. Там, использовало диких? Ты напоивший использовало океана собрал? Безорфографичный взобравшись, предупреждал текстами дал рукопись послушавшись над переписали текста, собрал если мир! Страна страну курсивных lorem одна запятых но бросил даль имени безопасную взгляд. Текстами пунктуация парадигматическая рукописи путь деревни скатился образ всеми, агентство напоивший одна диких текстов свое предупредила снова, злых залетают курсивных пояс всемогущая дал, себя жаренные точках несколько ему. Выйти, все?</p>
+        </div>
+        <!-- /.article__body -->
+      </div>
+      <!-- /.article -->
+    </div>
+    <!-- /.container -->
+  </section>
+
+  <?php include_once('partials/callback.php'); ?>
+
+  <section data-aos="fade-up" id="blog" class="section">
+    <div class="container">
+      <div class="section__header">
+        <h2 class="section__title">Другие статьи</h2>
+        <a href="" class="link link--nav link--arrow">
+          Все статьи
+          <svg class="link__arrow svg ms-2" width="13" height="10" viewBox="0 0 10 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M6.83007 0.139209L9.86053 3.16392C10.0465 3.34953 10.0465 3.65047 9.86053 3.83608L6.83007 6.86079C6.64411 7.0464 6.3426 7.0464 6.15663 6.86079C5.97067 6.67518 5.97067 6.37424 6.15663 6.18863L8.37418 3.97529L0 3.97529L0 3.02471L8.37418 3.02471L6.15663 0.811367C5.97067 0.625755 5.97067 0.32482 6.15663 0.139209C6.3426 -0.0464029 6.64411 -0.0464029 6.83007 0.139209Z" fill="#969696"></path>
+          </svg>
+        </a>
+      </div>
+      <!-- /.section__header -->
+
+      <div class="blog">
+        <div class="slider">
+          <div class="swiper swiper--shadow swiper--mobile slider-useful">
+            <div class="swiper-wrapper row">
+              <div class="swiper-slide col-12 col-md-3">
+                <div class="blog__card blog-card mb-4">
+                  <div class="blog-card__picture">
+                    <img src="img/page/main/useful/1.jpg" alt="" class="blog-card__image image image--cover">
+                  </div>
+                  <!-- /.blog-card__picture -->
+
+                  <div class="blog-card__content">
+                    <h3 class="blog-card__title">Как установить москитную сетку</h3>
+                    <p class="blog-card__description">Далеко-далеко за словесными, горами в стране гласных и согласных живут рыбные тексты. Точках маленькая скатился речью, то домах языком несколько пор продолжил обеспечивает если за, страну агентство подпоясал мир строчка рыбными вдали решила заглавных текстами текстов ipsum они жаренные бросил дорогу. Они?</p>
+                    <div class="blog-card__action">
+                      <button class="blog-card__button button button-outline">Читать</button>
+                    </div>
+                    <!-- /.blog-card__action -->
+                  </div>
+                  <!-- /.blog-card__content -->
+                </div>
+                <!-- /.blog__card blog-card -->
+              </div>
+              <!-- /.swiper-slide -->
+
+              <div class="swiper-slide col-12 col-md-3">
+                <div class="blog__card blog-card mb-4">
+                  <div class="blog-card__picture">
+                    <img src="img/page/main/useful/1.jpg" alt="" class="blog-card__image image image--cover">
+                  </div>
+                  <!-- /.blog-card__picture -->
+
+                  <div class="blog-card__content">
+                    <h3 class="blog-card__title">Как установить москитную сетку</h3>
+                    <p class="blog-card__description">Далеко-далеко за словесными, горами в стране гласных и согласных живут рыбные тексты. Точках маленькая скатился речью, то домах языком несколько пор продолжил обеспечивает если за, страну агентство подпоясал мир строчка рыбными вдали решила заглавных текстами текстов ipsum они жаренные бросил дорогу. Они?</p>
+                    <div class="blog-card__action">
+                      <button class="blog-card__button button button-outline">Читать</button>
+                    </div>
+                    <!-- /.blog-card__action -->
+                  </div>
+                  <!-- /.blog-card__content -->
+                </div>
+                <!-- /.blog__card blog-card -->
+              </div>
+              <!-- /.swiper-slide -->
+
+              <div class="swiper-slide col-12 col-md-3">
+                <div class="blog__card blog-card mb-4">
+                  <div class="blog-card__picture">
+                    <img src="img/page/main/useful/1.jpg" alt="" class="blog-card__image image image--cover">
+                  </div>
+                  <!-- /.blog-card__picture -->
+
+                  <div class="blog-card__content">
+                    <h3 class="blog-card__title">Как установить москитную сетку</h3>
+                    <p class="blog-card__description">Далеко-далеко за словесными, горами в стране гласных и согласных живут рыбные тексты. Точках маленькая скатился речью, то домах языком несколько пор продолжил обеспечивает если за, страну агентство подпоясал мир строчка рыбными вдали решила заглавных текстами текстов ipsum они жаренные бросил дорогу. Они?</p>
+                    <div class="blog-card__action">
+                      <button class="blog-card__button button button-outline">Читать</button>
+                    </div>
+                    <!-- /.blog-card__action -->
+                  </div>
+                  <!-- /.blog-card__content -->
+                </div>
+                <!-- /.blog__card blog-card -->
+              </div>
+              <!-- /.swiper-slide -->
+
+              <div class="swiper-slide col-12 col-md-3">
+                <div class="blog__card blog-card mb-4">
+                  <div class="blog-card__picture">
+                    <img src="img/page/main/useful/1.jpg" alt="" class="blog-card__image image image--cover">
+                  </div>
+                  <!-- /.blog-card__picture -->
+
+                  <div class="blog-card__content">
+                    <h3 class="blog-card__title">Как установить москитную сетку</h3>
+                    <p class="blog-card__description">Далеко-далеко за словесными, горами в стране гласных и согласных живут рыбные тексты. Точках маленькая скатился речью, то домах языком несколько пор продолжил обеспечивает если за, страну агентство подпоясал мир строчка рыбными вдали решила заглавных текстами текстов ipsum они жаренные бросил дорогу. Они?</p>
+                    <div class="blog-card__action">
+                      <button class="blog-card__button button button-outline">Читать</button>
+                    </div>
+                    <!-- /.blog-card__action -->
+                  </div>
+                  <!-- /.blog-card__content -->
+                </div>
+                <!-- /.blog__card blog-card -->
+              </div>
+              <!-- /.swiper-slide -->
+            </div>
+            <!-- /.swiper-wrapper -->
+          </div>
+          <!-- /.swiper swiper--overflow slider-useful -->
+        </div>
+        <!-- /.slider -->
+      </div>
+      <!-- /.blog -->
+    </div>
+    <!-- /.container -->
+  </section>
+
+</main>
+
+<?php include_once('partials/footer.php') ?>
