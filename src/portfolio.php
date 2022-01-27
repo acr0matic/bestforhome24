f92491fd21582be0bb256941d365b4a61404fff3
Nikita <main.acr0matic@gmail.com>
2022-01-27 19:40:31 +0300

    feat: скрипт для портфолио

--- src/portfolio.php
+++ src/portfolio.php
@@ -1,6 +1,6 @@
 <?php include_once('partials/header.php'); ?>
 
-<main id="portfolio">
+<main>
   <section class="section" id="portfolio">
     <div class="container">
       <div class="section__header">
@@ -14,89 +14,28 @@
             <div class="col-12 col-md-4 mb-4">
               <div class="portfolio__item">
                 <div class="portfolio__picture position-relative">
-                  <a data-fslightbox href="">
-                    <img src="img/misc/preloader.svg" class="preloader" alt="">
-                    <img data-src="img/portfolio/1.jpg" alt="" class="portfolio__image image image--cover lazy">
-                  </a>
+                  <img src="img/misc/preloader.svg" class="preloader" alt="">
+                  <img data-src="img/portfolio/1.jpg" alt="" class="portfolio__image image image--cover lazy">
                 </div>
                 <!-- /.portfolio__picture -->
 
                 <div class="portfolio__content">
+                  <ul class="list">
+                    <li class="list__item">Изделие: <span>Рулонные шторы</span></li>
+                    <li class="list__item">Цена: <span>11 999р</span></li>
+                    <li class="list__item">Ткань: <span>Нежный пурпуп (100%)</span></li>
+                    <li class="list__item">Конструкция: <span>Стандарт+</span></li>
+                  </ul>
 
-                </div>
-                <!-- /.portfolio__content -->
+                  <p class="text">
+                    Вне гласных и согласных живут рыбные тексты. Однажды переулка предупредила ты великий!
+                  </p>
 
-                <div class="portfolio__body">
-                  <h3 class="portfolio__title">Антикошка</h3>
-                  <div class="portfolio__wrapper">
-                    <div class="portfolio__info">
-                      Срок исполнения: <span>2 дня</span>
-                    </div>
-                    <!-- /.portfolio__info -->
-
-                    <svg class="portfolio-item__more" width="37" height="8" viewBox="0 0 37 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M6.6123 6.38086C5.93197 7.0612 5.09945 7.40137 4.11475 7.40137C3.13005 7.40137 2.29753 7.0612 1.61719 6.38086C0.936849 5.70052 0.59668 4.868 0.59668 3.8833C0.59668 2.8986 0.936849 2.06608 1.61719 1.38574C2.29753 0.705404 3.13005 0.365234 4.11475 0.365234C5.09945 0.365234 5.93197 0.705404 6.6123 1.38574C7.29264 2.06608 7.63281 2.8986 7.63281 3.8833C7.63281 4.868 7.29264 5.70052 6.6123 6.38086Z" fill="#ecd031"></path>
-                      <path d="M21.0605 6.38086C20.3802 7.0612 19.5477 7.40137 18.563 7.40137C17.5783 7.40137 16.7458 7.0612 16.0654 6.38086C15.3851 5.70052 15.0449 4.868 15.0449 3.8833C15.0449 2.8986 15.3851 2.06608 16.0654 1.38574C16.7458 0.705404 17.5783 0.365234 18.563 0.365234C19.5477 0.365234 20.3802 0.705404 21.0605 1.38574C21.7409 2.06608 22.0811 2.8986 22.0811 3.8833C22.0811 4.868 21.7409 5.70052 21.0605 6.38086Z" fill="#ecd031"></path>
-                      <path d="M35.5088 6.38086C34.8284 7.0612 33.9959 7.40137 33.0112 7.40137C32.0265 7.40137 31.194 7.0612 30.5137 6.38086C29.8333 5.70052 29.4932 4.868 29.4932 3.8833C29.4932 2.8986 29.8333 2.06608 30.5137 1.38574C31.194 0.705404 32.0265 0.365234 33.0112 0.365234C33.9959 0.365234 34.8284 0.705404 35.5088 1.38574C36.1891 2.06608 36.5293 2.8986 36.5293 3.8833C36.5293 4.868 36.1891 5.70052 35.5088 6.38086Z" fill="#ecd031"></path>
-                    </svg>
+                  <div class="gallery">
+                    <img data-full="" src="" alt="">
+                    <img data-full="" src="" alt="">
+                    <img data-full="" src="" alt="">
                   </div>
-                  <!-- /.portfolio__wrapper -->
-                </div>
-                <!-- /.portfolio__body -->
-              </div>
-              <!-- /.portfolio__item -->
-            </div>
-            <!-- /.col-4 -->
-
-            <div class="col-12 col-md-4 mb-4">
-              <div class="portfolio__item">
-                <div class="portfolio__picture position-relative">
-                  <a data-fslightbox href="">
-                    <img src="img/misc/preloader.svg" class="preloader" alt="">
-                    <img data-src="img/portfolio/1.jpg" alt="" class="portfolio__image image image--cover lazy">
-                  </a>
-                </div>
-                <!-- /.portfolio__picture -->
-
-                <div class="portfolio__content">
-
-                </div>
-                <!-- /.portfolio__content -->
-
-                <div class="portfolio__body">
-                  <h3 class="portfolio__title">Антикошка</h3>
-                  <div class="portfolio__wrapper">
-                    <div class="portfolio__info">
-                      Срок исполнения: <span>2 дня</span>
-                    </div>
-                    <!-- /.portfolio__info -->
-
-                    <svg class="portfolio-item__more" width="37" height="8" viewBox="0 0 37 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M6.6123 6.38086C5.93197 7.0612 5.09945 7.40137 4.11475 7.40137C3.13005 7.40137 2.29753 7.0612 1.61719 6.38086C0.936849 5.70052 0.59668 4.868 0.59668 3.8833C0.59668 2.8986 0.936849 2.06608 1.61719 1.38574C2.29753 0.705404 3.13005 0.365234 4.11475 0.365234C5.09945 0.365234 5.93197 0.705404 6.6123 1.38574C7.29264 2.06608 7.63281 2.8986 7.63281 3.8833C7.63281 4.868 7.29264 5.70052 6.6123 6.38086Z" fill="#ecd031"></path>
-                      <path d="M21.0605 6.38086C20.3802 7.0612 19.5477 7.40137 18.563 7.40137C17.5783 7.40137 16.7458 7.0612 16.0654 6.38086C15.3851 5.70052 15.0449 4.868 15.0449 3.8833C15.0449 2.8986 15.3851 2.06608 16.0654 1.38574C16.7458 0.705404 17.5783 0.365234 18.563 0.365234C19.5477 0.365234 20.3802 0.705404 21.0605 1.38574C21.7409 2.06608 22.0811 2.8986 22.0811 3.8833C22.0811 4.868 21.7409 5.70052 21.0605 6.38086Z" fill="#ecd031"></path>
-                      <path d="M35.5088 6.38086C34.8284 7.0612 33.9959 7.40137 33.0112 7.40137C32.0265 7.40137 31.194 7.0612 30.5137 6.38086C29.8333 5.70052 29.4932 4.868 29.4932 3.8833C29.4932 2.8986 29.8333 2.06608 30.5137 1.38574C31.194 0.705404 32.0265 0.365234 33.0112 0.365234C33.9959 0.365234 34.8284 0.705404 35.5088 1.38574C36.1891 2.06608 36.5293 2.8986 36.5293 3.8833C36.5293 4.868 36.1891 5.70052 35.5088 6.38086Z" fill="#ecd031"></path>
-                    </svg>
-                  </div>
-                  <!-- /.portfolio__wrapper -->
-                </div>
-                <!-- /.portfolio__body -->
-              </div>
-              <!-- /.portfolio__item -->
-            </div>
-            <!-- /.col-4 -->
-
-            <div class="col-12 col-md-4">
-              <div class="portfolio__item">
-                <div class="portfolio__picture position-relative">
-                  <a data-fslightbox href="">
-                    <img src="img/misc/preloader.svg" class="preloader" alt="">
-                    <img data-src="img/portfolio/1.jpg" alt="" class="portfolio__image image image--cover lazy">
-                  </a>
-                </div>
-                <!-- /.portfolio__picture -->
-
-                <div class="portfolio__content">
-
                 </div>
                 <!-- /.portfolio__content -->
 
@@ -175,7 +114,7 @@
 
         <div class="modal__section">
           <h3 class="modal__subtitle">Фотографии:</h3>
-          <div class="row">
+          <div class="row modal__gallery">
             <div class="col-4">
               <div class="modal__picture">
                 <a href="" data-fslightbox="">
---
+++ src/scripts/portfolio.js
@@ -0,0 +1,42 @@
+const portfolio = document.getElementById('portfolio');
+
+if (portfolio) {
+  const modal = document.getElementById('modal-portfolio');
+
+  const cards = portfolio.querySelectorAll('.portfolio__item');
+  _.forEach(cards, (card) => {
+    const list = card.querySelector('.portfolio__content .list').innerHTML;
+    const text = card.querySelector('.portfolio__content .text').innerHTML;
+    const images = card.querySelectorAll('.portfolio__content .gallery img');
+
+    card.addEventListener('click', () => {
+      UpdateModal(modal, list, text, images);
+      MicroModal.show('modal-portfolio', modalParams);
+    });
+  });
+
+  function UpdateModal(modal, list, text, images) {
+    const parameters = modal.querySelector('.modal__list');
+    const description = modal.querySelector('.modal__description');
+    const gallery = modal.querySelector('.modal__gallery');
+
+    parameters.innerHTML = list;
+    description.innerHTML = text;
+    gallery.innerHTML = '';
+    
+    _.forEach(images, (image) => {
+      gallery.innerHTML +=
+        `
+        <div class="col-4">
+          <div class="modal__picture">
+              <a href="${image.dataset.full}" data-fslightbox="">
+                <img src="${image.src}" alt="" class="modal__image image image--cover">
+              </a>
+            </div>
+            <!-- /.modal__picture -->
+          </div>
+        <!-- /.col-4 -->
+        `;
+    });
+  }
+}
